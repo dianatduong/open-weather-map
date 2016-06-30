40eca1a97a567ddec1e3b67a502d67309ce0463b
Diana Duong <dduong013@gmail.com>
2016-06-30 14:18:15 -0500

    changed location

--- app/owm-app.js
+++ app/owm-app.js
@@ -3,6 +3,7 @@ angular
 	.module('OWMApp', ['ngRoute'])
 	//$routeProvider is used to set URL routing rules
 	.config(['$routeProvider', function($routeProvider){
+		//$routeProvider.when() is used to specify that when users request the root url, the app should respond with the home.html template and the home controller
 		$routeProvider.when('/', {
 			templateUrl : 'home.html',
 			controller : 'HomeCtrl'

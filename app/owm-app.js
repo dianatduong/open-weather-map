7ba644efe09d0921abb60fa7e68327bf25275d38
Diana Duong <dduong013@gmail.com>
2016-07-08 12:37:22 -0500

    modified .run route change on start and success

--- app/owm-app.js
+++ app/owm-app.js
@@ -21,6 +21,8 @@ angular
 			// resolve property will preload the city before the controller and template run so that when a user provide an input for an unsupported city, the city view would not be loaded
 			//the view will not be rendered until the city dependency is resolved
 			//resolvers act as a middleware before a view is rendered, a promise can be returned instead of an actual value if something asynchonous needs to be fetched
+			//forces dependencies to resolve before the route is displayed.
+			//resolve properties can be then injected into the controller
 			resolve : {
 				city: function(owmCities, $route, $location) {
 					var city = $route.current.params.city;
@@ -28,6 +30,7 @@ angular
 					if(owmCities.indexOf(city) == -1 ){
 						//$location service will redirect to the error page when a matching city is not found
 						//does the matching to change the URL 
+						//reroutes user to an error page
 						$location.path('/error'); 
 						return;
 					}
@@ -35,15 +38,28 @@ angular
 				}
 			}
 		})
+		// when error display template message
 		.when('/error', {
 			//this template property supplies an in-line template. This practice should be avoided because it mixes template and JS code 
 			template : '<p>Error - Page Not Found</p>' 
 		})
+		//otherwise() is a fallback method to catch any routes that havent been specified
 		.otherwise('/error');
 	}])
-	.run(['$rootScope', '$location', function($rootScope, $location){
+	.run(['$rootScope', '$location', '$timeout', function($rootScope, $location, $timeout){
+		//when there is a route change error, reroute the user to the error path
 		$rootScope.$on('$routeChangeError', function(){
 			$location.path('/error');
+		});
+		// when the route change starts, set the root scope 'isLoading' variable to true
+		$rootScope.$on('$routeChangeStart', function(){
+			$rootScope.isLoading = true;
+		});
+		//when route change succeeds, wait 1 sec and then set isLoading back to false
+		$rootScope.$on('$routeChangeSuccess', function(){
+			$timeout(function(){
+				$rootScope.isLoading = false;
+			}, 1000);
 		});
 	}])
 	//controller is defined as HomeCtrl
